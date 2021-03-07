060fb29996f0f61c36082760d101f0dc1965cf4f
Marten Seemann <martenseemann@gmail.com>
2021-03-07 23:21:15 +0800

    add a workflow to check that 3rd party actions are included by hash (#20)
    * add a workflow to check that 3rd party actions are included by hash
    
    * simplify check-3rd-party.sh
    
    * use double brackets for if in bash
    
    * use a regex to check the SHA1 commit hash

---
+++ .github/workflows/check-3rd-party.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE=$1
+status=0
+
+for line in `sed -ne 's/[[:space:]-]*uses:[[:space:]]*//p' $1 | sed -e 's/\s*#.*$//'`; do
+  author=`echo $line | awk -F/ '{print $1}'`
+  if [[ $author == "actions" ]]; then continue; fi
+  version=`echo $line | awk -F@ '{print $2}' | awk '{print $1}'`
+  if ! [[ "$version" =~ ^[a-f0-9]{40}$ ]]; then
+    status=1
+    echo "$FILE includes $line and doesn't use commit hash"
+  fi
+done 
+
+exit $status
