7d7b567e9aa45b855c3c4f3deb6882ca8301b504
Piotr Galar <piotr.galar@gmail.com>
2023-07-28 13:07:35 +0100

    feat: by default perform dependabot-like updates only (#556)
    * feat: by default perform dependabot-like updates only
    
    * feat: skip web3-bot updates if dependabot is configured
    
    ---------
    
    Co-authored-by: dependabot <dependabot@github.com>

--- scripts/copy-templates.sh
+++ scripts/copy-templates.sh
@@ -2,35 +2,69 @@
 
 set -euo pipefail -o nounset
 
+force="$(jq -r '.force' <<< "$CONTEXT")"
+
+root="$(pwd)"
+
+pushd "$REPO" > /dev/null
+
 for f in $(jq -r '.config.files[]' <<< "$CONTEXT"); do
-  echo -e "\nProcessing $f."
-  # add DO NOT EDIT header
-  tmp=$(mktemp)
-  cat protocol/.github/templates/header.yml protocol/.github/templates/$f > $tmp
-  # render template
-  ./protocol/.github/scripts/render-template.sh "$tmp" "$CONTEXT" "$tmp"
-  # create commit, if necessary
-  commit_msg=""
-  if [[ ! -f "$REPO/$f" ]]; then
-    echo "First deployment.\n"
-    commit_msg="chore: add $f"
-  elif [[ "$f" == "version.json" ]]; then
-    echo "Version file. Skipping."
+  if [[ -f "$f" && "$force" != "true" ]]; then
+    echo "$f already exists. Skipping."
     continue
-  else
-    status=$(cmp --silent $REPO/$f $tmp; echo $?)
-    if [[ $status -ne 0 ]]; then
-      echo "Update needed."
-      commit_msg="chore: update $f"
-    else
-      echo "File identical. Skipping."
-      continue
+  fi
+
+  echo "Rendering template..."
+  $root/protocol/.github/scripts/render-template.sh "$root/protocol/.github/templates/$f" "$CONTEXT" "$f"
+
+  git add "$f"
+  git commit -m "chore: add or force update $f"
+done
+
+if [[ "$force" != "true" ]]; then
+  if [[ -f ".github/dependabot.yml" ]]; then
+    gha="$(yq '.updates | map(select(.package-ecosystem == "github-actions")) | length' .github/dependabot.yml)"
+    if [[ "$gha" -gt 0 ]]; then
+      echo "Dependabot is already configured to update GitHub Actions. Skipping."
+      exit 0
     fi
   fi
-  mkdir -p "$REPO/$(dirname $f)"
-  mv $tmp $REPO/$f
-  pushd $REPO > /dev/null
-  git add $f
-  git commit -m "$commit_msg"
-  popd > /dev/null
-done
+
+  # https://gist.github.com/mattt/e09e1ecd76d5573e0517a7622009f06f
+  gh gist view --raw e09e1ecd76d5573e0517a7622009f06f | bash
+
+  tmp="$(mktemp)"
+
+  dependabot update github_actions  "$REPO" --local "$REPO" --output "$tmp"
+
+  branch="$(git branch --show-current)"
+  sha="$(git rev-parse HEAD)"
+
+  for pr in $(yq -c '.output | map(select(.type == "create_pull_request")) | .[]' "$tmp"); do
+    title="$(jq -r '.pr-title' <<< "$pr")"
+    git checkout -b "$title" "$branch"
+    for f in $(jq -r '.updated-dependency-files[]' <<< "$pr"); do
+      jq -r '.content' <<< "$f" > "$REPO/$(jq -r '.name' <<< "$f")"
+    done
+    git add .
+    git commit -m "$(jq -r '.commit-message' <<< "$pr")"
+    git checout "$branch"
+    git merge "$title" --strategy-option theirs
+  done
+
+  git reset "$sha"
+
+  for f in $(jq -r '.config.files[]' <<< "$CONTEXT"); do
+    if [[ ! -f "$REPO/$f" ]]; then
+      echo "$f does not exist. Skipping."
+      continue
+    fi
+
+    git add "$f"
+    git commit -m "chore: update $f"
+  done
+
+  git reset --hard
+fi
+
+popd > /dev/null
